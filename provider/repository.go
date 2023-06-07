e0dd6b4645ff886770391678688e1cf06b1f294e
Mathis Albertzard <malbertzard@gmail.com>
2023-06-07 23:17:47 +0200

    feat: Implemented Git provider; wip

--- provider/repository.go
+++ provider/repository.go
@@ -5,132 +5,139 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
+
+	"github.com/malbertzard/repowatcher/model"
 )
 
-//TODO Git Provider
-
 type RepoStorage interface {
-	PullRepos() error
-	DiffAllRepos() ([]string, error)
-	DiffRepo(path string) (string, error)
+    PullRepos() error
+    DiffAllRepos() ([]string, error)
+    DiffRepo(path string) (string, error)
 }
 
-type GitRepoStorage struct {
-	basePath string
+type GitRepoStorage struct{
+    Repos []model.Repo
 }
 
-func (g *GitRepoStorage) PullRepos() error {
-	paths, err := getDirsFromPath(g.basePath)
-	if err != nil {
-		return err
-	}
+func (g GitRepoStorage) PullRepos() error {
+    // Iterate over the repos and pull the Git repositories
+    for _, repo := range g.Repos {
+        if err := g.pullRepo(repo.Path); err != nil {
+            return err
+        }
+    }
 
-	for _, path := range paths {
-        err = pullRepo(g.basePath + path)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+    return nil
 }
 
-func (g *GitRepoStorage) DiffAllRepos() ([]string, error) {
-	paths, err := getDirsFromPath(g.basePath)
-	if err != nil {
-		return nil, err
-	}
+func (g GitRepoStorage) DiffAllRepos() ([]string, error) {
+	var diffs []string
 
-	var diffList []string
-	var bytes []byte
-	for _, path := range paths {
-		repoPath := g.basePath + path
-		lastCommit := getLastCommitHash(repoPath)
-
-		bytes, err = getDiff(lastCommit, repoPath)
-
+	for _, repo := range g.Repos {
+		diff, err := g.diffRepo(repo.Path)
 		if err != nil {
 			return nil, err
 		}
-		diffList = append(diffList, string(bytes))
-		if err != nil {
-			return nil, err
+		if diff != "" {
+			diffs = append(diffs, diff)
 		}
 	}
-	return diffList, nil
+
+	return diffs, nil
 }
 
-func (g *GitRepoStorage) DiffRepo(path string) (string, error) {
-	repoPath := g.basePath + path
-	lastCommit := getLastCommitHash(repoPath)
+func (g GitRepoStorage) DiffRepo(repo model.Repo) (string, error) {
+    return g.diffRepo(repo.Path)
+}
 
-	bytes, err := getDiff(lastCommit, repoPath)
+func (g GitRepoStorage) DiffRepoToCommit(repo model.Repo, lastCommit string) (string, error) {
+    return g.diffRepo(repo.Path)
+}
 
-	if err != nil {
-		return "", err
-	}
-	return string(bytes), nil
+func (g GitRepoStorage) pullRepo(path string) error {
+    cmd := exec.Command("git", "-C", path, "pull")
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    return cmd.Run()
+}
+
+func (g GitRepoStorage) diffRepo(path string) (string, error) {
+    cmd := exec.Command("git", "-C", path, "diff")
+    output, err := cmd.Output()
+    if err != nil {
+        if exitError, ok := err.(*exec.ExitError); ok {
+            // Git diff returns a non-zero exit code if there are no changes
+            if exitError.ExitCode() == 1 {
+                return "", nil
+            }
+        }
+        return "", err
+    }
+
+    return strings.TrimSpace(string(output)), nil
 }
 
 //Helper functions
 
 func pullRepo(path string) error {
-	cmd := exec.Command("git", "pull")
-	cmd.Dir = path
-	err := cmd.Run()
-	if err != nil {
-		return err
-	}
+    cmd := exec.Command("git", "pull")
+    cmd.Dir = path
+    err := cmd.Run()
+    if err != nil {
+        return err
+    }
     return nil
 }
 
 func getLastCommitHash(path string) string {
-	cmd := exec.Command("git", "rev-parse", "HEAD")
-	cmd.Dir = path
-	out, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return bytes.NewBuffer(out).String()
+    cmd := exec.Command("git", "rev-parse", "HEAD")
+    cmd.Dir = path
+    out, err := cmd.Output()
+    if err != nil {
+        log.Fatal(err)
+    }
+    return bytes.NewBuffer(out).String()
 }
 
 func getDiffFiles(LastCommit string, path string) []byte {
-	cmd := exec.Command("git", "diff", LastCommit, "HEAD", "--name-only")
-	cmd.Dir = path
-	out, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return out
+    cmd := exec.Command("git", "diff", LastCommit, "HEAD", "--name-only")
+    cmd.Dir = path
+    out, err := cmd.Output()
+    if err != nil {
+        log.Fatal(err)
+    }
+    return out
 }
 
 func getDiff(LastCommit string, path string) ([]byte, error) {
-	cmd := exec.Command("git", "diff", LastCommit, "HEAD", "--color")
-	cmd.Dir = path
-	out, err := cmd.Output()
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
+    cmd := exec.Command("git", "diff", LastCommit, "HEAD", "--color")
+    cmd.Dir = path
+    out, err := cmd.Output()
+    if err != nil {
+        return nil, err
+    }
+    return out, nil
 }
 
 func getDirsFromPath(dirPath string) ([]string, error) {
-	dir, err := os.Open(dirPath)
-	if err != nil {
-		return nil, err
-	}
-	defer dir.Close()
+    dir, err := os.Open(dirPath)
+    if err != nil {
+        return nil, err
+    }
+    defer dir.Close()
 
-	files, err := dir.ReadDir(-1) // -1 means read all entries
-	if err != nil {
-		return nil, err
-	}
+    files, err := dir.ReadDir(-1) // -1 means read all entries
+    if err != nil {
+        return nil, err
+    }
 
-	var folders []string
-	for _, file := range files {
-		if file.IsDir() {
-			folders = append(folders, file.Name())
-		}
-	}
+    var folders []string
+    for _, file := range files {
+        if file.IsDir() {
+            folders = append(folders, file.Name())
+        }
+    }
 
-	return folders, nil
+    return folders, nil
 }
