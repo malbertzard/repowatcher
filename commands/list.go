d4220c51f1283e78873725a4950cd2368f4a342f
Mathis Albertzard <malbertzard@gmail.com>
2023-06-26 14:23:45 +0200

    feat: Made list command better looking

--- commands/list.go
+++ commands/list.go
@@ -1,16 +1,21 @@
 package commands
 
 import (
-	"fmt"
+	"os"
 	"repo-watch/models"
 	"repo-watch/receiver"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 func ListRepositories(config *models.Config, receiver *receiver.Receiver) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Nickname", "URL", "Folder Name"})
+
 	for _, repo := range config.Repositories {
-		fmt.Printf("Nickname: %s\n", repo.Nickname)
-		fmt.Printf("URL: %s\n", repo.URL)
-		fmt.Printf("Folder Name: %s\n", repo.FolderName)
-		fmt.Println("----------------------")
+		row := []string{repo.Nickname, repo.URL, repo.FolderName}
+		table.Append(row)
 	}
+
+	table.Render() // Output table
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
